c64fcbc1ac8c1eddbe4465182526a2dd560cbd1e
Jerome Kelleher <jk@well.ox.ac.uk>
2015-05-31 18:11:27 +0100

    Added travis config and unit tests outline.

---
+++ tests.py
@@ -0,0 +1,31 @@
+#
+# Copyright (C) 2015 Jerome Kelleher <jerome.kelleher@well.ox.ac.uk>
+#
+# This file is part of kingman.
+#
+# kingman is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# kingman is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with kingman.  If not, see <http://www.gnu.org/licenses/>.
+#
+"""
+Unit tests for the kingman package. These are intended to be run
+using nose.
+"""
+from __future__ import print_function
+from __future__ import division
+
+import unittest
+
+class TestCli(unittest.TestCase):
+    """
+    Test cases for the command line interface for kingman.
+    """
