c15fe18534acf94cfde9d8b150174cd345a3422d
Jerome Kelleher <jk@well.ox.ac.uk>
2015-06-01 11:12:56 +0100

    Finished docs.

--- docs/cli.rst
+++ docs/cli.rst
@@ -5,3 +5,9 @@ Command line interface
 This is the documentation for the ``kingman`` program, a simple
 command line interface to the simulations for the Kingman
 coalescent.
+
+.. note::
+
+    **TODO:** Find good sphinx based method for documenting a CLI.
+    We should be able to import the command line parser from
+    the kingman package, and generated the documentaion from this.
--- kingman/simulator.py
+++ kingman/simulator.py
@@ -29,6 +29,23 @@ def simulate(sample_size, random_seed=None):
     """
     Simulates the Kingman coalescent for the specified sample size
     and random seed.
+
+    Returns a tuple (parent, time) which describes the simulated
+    history of the sample as an oriented forest. ``parent`` is a
+    list of integers, in which the parent of node ``j`` is
+    ``parent[j]``. ``time`` an array of floating point values
+    in which the time at which node ``j`` was created is ``time[j]``. See
+    `<http://jeromekelleher.github.io/ercs/#oriented-trees-and-forests>`_
+    for further information on oriented forests.
+
+    Time is measured in units of 4Ne generations,
+    following Hudson's ms.
+
+    :param sample_size: The sample size; must be >= 2.
+    :param random_seed: The random seed for simulations
+    :type sample_size: integer
+    :type random_seed: integer or None
+    :rtype: (list, list)
     """
     if sample_size < 2:
         raise ValueError("sample_size must be >= 2")
